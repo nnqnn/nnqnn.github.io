579c496f7948341991d4474007e033ca191852b0
Avtohauser <147597796+avtohauser@users.noreply.github.com>
2024-02-18 01:09:37 +0300

    V2
    completely redesigned website

---
+++ script.js
@@ -0,0 +1,12 @@
+// Активировать темную тему при загрузке страницы
+toggleTheme('dark');
+
+function toggleTheme(theme) {
+    const body = document.body;
+    if (theme === 'light') {
+        body.classList.replace('dark-theme', 'light-theme');
+    } else {
+        body.classList.replace('light-theme', 'dark-theme');
+    }
+}
+
