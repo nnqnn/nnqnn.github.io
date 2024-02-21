283a7a34b9638bfd23555b409da294d4157cebf9
[AVR] <sigma@avto.ru>
2024-02-21 18:39:35 +0300

    v3.0.4

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function openNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+function toggleTheme(theme) {
+    document.body.className = theme + '-theme';
+}
