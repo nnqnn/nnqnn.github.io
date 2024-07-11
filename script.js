0d87a537b545a272bc23fe03c0c09ee9ecad5553
nnqnn <kalimullinkam@gmail.com>
2024-07-11 22:44:29 +0300

    более менее большое обновление сайта, добавлены новые достижения, добавил работу в команде таптаб, убраны темы и другие небольшие корректировки и улучшения

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Активировать темную тему при загрузке страницы
-toggleTheme('dark');
-
-function toggleTheme(theme) {
-    const body = document.body;
-    if (theme === 'light') {
-        body.classList.replace('dark-theme', 'light-theme');
-    } else {
-        body.classList.replace('light-theme', 'dark-theme');
-    }
-}
-
 function openNav() {
     document.getElementById("sidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -29,8 +17,3 @@ function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
-
-function toggleTheme(theme) {
-    document.body.className = theme + '-theme';
-}
-
